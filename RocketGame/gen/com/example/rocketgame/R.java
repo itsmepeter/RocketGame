f3c4dc265e383abf16f7e6f2354e921193e70ff1
itsmepeter <PeterDeKempeneer@hotmail.com>
2013-11-22 09:22:53 +0100

    kleurrorod

--- RocketGame/gen/com/example/rocketgame/R.java
+++ RocketGame/gen/com/example/rocketgame/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int MenuBackgroundColor=0x7f060000;
+        public static final int MenuHeaderBackgroundColor=0x7f060001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
