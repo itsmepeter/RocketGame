743e6fb754ea0abbb4a6526a4b302afaf8c6c96a
itsmepeter <PeterDeKempeneer@hotmail.com>
2013-11-22 09:50:05 +0100

    Shop basic ui

--- RocketGame/gen/com/example/rocketgame/R.java
+++ RocketGame/gen/com/example/rocketgame/R.java
@@ -29,11 +29,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int TextView2=0x7f090005;
+        public static final int action_settings=0x7f090008;
         public static final int btnAchievements=0x7f090003;
         public static final int btnHighScores=0x7f090004;
+        public static final int btnMenuStart=0x7f090001;
         public static final int btnOptions=0x7f090002;
-        public static final int btnStart=0x7f090001;
+        public static final int btnShopBack=0x7f090006;
+        public static final int btnShopStart=0x7f090007;
         public static final int textView1=0x7f090000;
     }
     public static final class layout {
