a5d9d1a214d6d149941a5b78c9cbbcfd41ddda7d
itsmepeter <PeterDeKempeneer@hotmail.com>
2013-11-22 09:42:40 +0100

    rere

--- RocketGame/gen/com/example/rocketgame/R.java
+++ RocketGame/gen/com/example/rocketgame/R.java
@@ -38,6 +38,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_menu=0x7f030000;
+        public static final int activity_shop=0x7f030001;
     }
     public static final class menu {
         public static final int menu=0x7f080000;
